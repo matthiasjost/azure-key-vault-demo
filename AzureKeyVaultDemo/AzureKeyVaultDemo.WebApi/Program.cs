e2ee7b6e3cd8a39249d0dafe71fce7b80eef70a7
Matthias Jost <8178852+matthiasjost@users.noreply.github.com>
2022-11-05 11:45:28 +0100

    Another test

--- AzureKeyVaultDemo/AzureKeyVaultDemo.WebApi/Program.cs
+++ AzureKeyVaultDemo/AzureKeyVaultDemo.WebApi/Program.cs
@@ -3,6 +3,15 @@ using Azure.Identity;
 
 namespace AzureKeyVaultDemo.WebApi
 {
+    // Helpful articles:
+
+    // How to Use Azure Key Vault With an Azure Web App in C#:
+    // https://blog.loginradius.com/engineering/guest-post/using-azure-key-vault-with-an-azure-web-app-in-c-sharp/
+
+    // Azure Key Vault configuration provider in ASP.NET Core
+    // https://learn.microsoft.com/en-us/aspnet/core/security/key-vault-configuration?view=aspnetcore-6.0
+    // Repository: https://github.com/dotnet/AspNetCore.Docs/blob/main/aspnetcore/security/key-vault-configuration/samples/6.x/KeyVaultConfigurationSample/Program.cs
+
     public class Program
     {
         public static void Main(string[] args)
